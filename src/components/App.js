bda8562be52fe21a9e02bf695df24d616cac4155
claudiabg-c <claudia.berrocalgomez@gmail.com>
2022-03-17 21:29:33 +0100

    Add basic HTML structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,31 @@
 const App = () => {
-  return <div>Hola mundo</div>;
+  return (
+    <div>
+      <header>
+        <nav>
+          <div>
+            <h1>Gloria Gómez-Calcerrada</h1>
+            <h2>Pintora</h2>
+          </div>
+          <div>
+            <h3>Sobre mi</h3>
+            <h3>Galería</h3>
+            <h3>Exposiciones</h3>
+            <h3>Otros trabajos</h3>
+            <h3>Contacto</h3>
+            <h3>Carro</h3>
+          </div>
+        </nav>
+      </header>
+      <main></main>
+      <footer>
+        <span>
+          &copy; 2022 Gloria Gómez-Calcerrada Pintora. Todos los derechos
+          reservados
+        </span>
+      </footer>
+    </div>
+  );
 };
 
 export default App;
