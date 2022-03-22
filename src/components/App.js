dfc068313fb010235098a66c3d4f88e403aa0cf1
claudiabg-c <claudia.berrocalgomez@gmail.com>
2022-03-22 23:11:27 +0100

    Work on nav bar. Add some styles.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
+import "../styles/App.scss";
+import "../styles/Reset.scss";
+
 const App = () => {
   return (
     <div>
       <header>
-        <nav>
-          <div>
-            <h1>Gloria Gómez-Calcerrada</h1>
-            <h2>Pintora</h2>
+        <nav className="nav row">
+          <div className="titlebackground col">
+            <h1 className="nameartist">
+              <a href="#">Gloria Gómez-Calcerrada</a>
+            </h1>
           </div>
-          <div>
-            <h3>Sobre mi</h3>
-            <h3>Galería</h3>
-            <h3>Exposiciones</h3>
-            <h3>Otros trabajos</h3>
-            <h3>Contacto</h3>
-            <h3>Carro</h3>
+          <div className="col">
+            <div>
+              <h3>Sobre mí</h3>
+              <h3>Galería</h3>
+              <h3>Exposiciones</h3>
+              <h3>Otros trabajos</h3>
+              <h3>Contacto</h3>
+              <h3>Carro</h3>
+            </div>
           </div>
         </nav>
       </header>
