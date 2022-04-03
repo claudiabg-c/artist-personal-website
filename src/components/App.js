e0289ee3bfdc905582c4edc0158185450e55596d
claudiabg-c <claudia.berrocalgomez@gmail.com>
2022-04-03 23:43:05 +0200

    Add interactivity to burger menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import "../styles/App.scss";
 import "../styles/Reset.scss";
 
 const App = () => {
+  const [burger, setBurger] = useState("false");
+  const [dNone, setDNone] = useState("true");
+
+  function toggleBurgerClass() {
+    setBurger(!burger);
+    setDNone(!dNone);
+  }
+
   return (
     <div>
       <header>
-        <nav className="nav row">
-          <div className="titlebackground col">
+        <nav className="nav row-no-gutters">
+          <div className={`titlebackground col${burger ? "-12" : "-6"}`}>
             <h1 className="nameartist">
               <a href="#">Gloria Gómez-Calcerrada</a>
             </h1>
           </div>
-          <div className="col d-none">
+          <div
+            className={`menu-toggle ${burger ? "" : "open"}`}
+            onClick={toggleBurgerClass}
+          >
+            <div className="hamburger" onClick={toggleBurgerClass}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className="cross">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <div className={`menu ${dNone ? "d-none" : ""}`}>
             <div>
               <h3>Sobre mí</h3>
               <h3>Galería</h3>
