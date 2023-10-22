008e7745b1ce3ed10e2001673895359dce3e8c4a
Claudia Berrocal <claudia.berrocalgomez@gmail.com>
2023-10-22 13:37:08 +0200

    Add switch routes. Add Biography as separate component. Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Route, Switch, Link, useHistory } from "react-router-dom";
+import Gallery from "./Gallery"
+import Biography from "./Biography"
 import "../styles/App.scss";
 import "../styles/Reset.scss";
 
@@ -6,9 +9,19 @@ const App = () => {
   const [burger, setBurger] = useState("false");
   const [dNone, setDNone] = useState("true");
 
+  const history = useHistory();
+
   function toggleBurgerClass() {
     setBurger(!burger);
     setDNone(!dNone);
+  }
+
+  function scrollToSection(sectionId) {
+    history.push('/');
+    const sectionElement = document.getElementById(sectionId);
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
   return (
@@ -38,10 +51,12 @@ const App = () => {
               </div>
             </div>
             <div className={`menu ${dNone ? "d-none" : ""}`} onClick={toggleBurgerClass}>
-              <h3><a href="#biography">Sobre la artista</a></h3>
-              <h3><a href="#exhibits">Exposiciones</a></h3>
-              <h3><a href="#">Galería</a></h3>
-              <h3><a href="#">Otros trabajos</a></h3>
+              <h3 onClick={() => scrollToSection("biography")}>Sobre la artista</h3>
+              <h3 onClick={() => scrollToSection("exhibits")}>Exposiciones</h3>
+              <Link to="/gallery">
+                <h3>Galería</h3>
+              </Link>
+              <h3><a href="#other-jobs">Otros trabajos</a></h3>
               <h3><a href="#">Contacto</a></h3>
               <h3><a href="#">Carro</a></h3>
             </div>
@@ -49,21 +64,10 @@ const App = () => {
         </nav>
       </header>
       <main>
-        <section id="biography">
-          <h1>Biografía</h1>
-          <p>Culpa minim aliqua mollit sit aute incididunt in deserunt elit consequat quis pariatur. In pariatur consequat aliquip adipisicing sunt. Nisi occaecat adipisicing Lorem qui. Id eiusmod adipisicing qui ut amet qui est esse pariatur magna esse cillum.</p>
-          <p>Eu cupidatat ullamco velit non consequat exercitation qui aliqua fugiat aute. Excepteur deserunt do in laboris officia velit proident ut. Consectetur aliqua et excepteur ea ullamco non eu. Nostrud ex aliqua irure tempor ullamco fugiat excepteur aliqua occaecat anim excepteur nostrud nostrud pariatur.</p>
-          <p>Anim culpa adipisicing quis tempor. Eiusmod deserunt anim sunt duis ad laboris eiusmod incididunt. Nostrud ullamco minim ad tempor minim commodo ipsum eu qui duis do.</p>
-          <p>Veniam tempor deserunt pariatur mollit excepteur aute. Ex Lorem velit quis nostrud ad nulla irure adipisicing ut ea proident occaecat ad esse. Est eu elit esse excepteur minim est qui aute est tempor ea aliqua et do. Sunt ea magna aliquip eiusmod excepteur duis veniam ullamco. Pariatur veniam nulla excepteur mollit eiusmod voluptate enim ea. Ad labore culpa ullamco enim pariatur. Est ad elit cupidatat et excepteur aliquip sit qui.</p>
-          <p>Voluptate magna eu esse in cillum laborum do elit aliqua. Aliqua ex culpa occaecat ut id ullamco minim fugiat qui nulla. Irure labore eu veniam anim eiusmod veniam laborum dolor velit nostrud ullamco ea labore. Cupidatat commodo deserunt veniam eu consectetur deserunt eiusmod labore excepteur. Lorem proident sunt do deserunt aliquip. Adipisicing nulla eu exercitation commodo ad id quis officia Lorem voluptate laboris sunt est aute.</p>
-        </section>
-        <section id="exhibits">
-          <h1>Exposiciones</h1>
-          <p><span className="year">2018</span> - Ullamco culpa quis labore est.</p>
-          <p><span className="year">2017</span> - Cillum sit ut pariatur aute magna excepteur labore nostrud elit.</p>
-          <p><span className="year">2015</span> - Consectetur deserunt aliqua ipsum esse sunt culpa eu ullamco.</p>
-          <p><span className="year">2010</span> - Tempor mollit ipsum Lorem ipsum incididunt ea occaecat est.</p>
-        </section>
+        <Switch>
+          <Route path="/" exact component={Biography}/>
+          <Route path="/gallery" component={Gallery}/>
+        </Switch>
       </main>
       <footer>
         <span>
---
+++ src/components/Biography.js
@@ -0,0 +1,23 @@
+function Biography() {
+    return (
+        <section className="biography-and-exibits">
+            <section id="biography">
+            <h1>Biografía</h1>
+            <p>Culpa minim aliqua mollit sit aute incididunt in deserunt elit consequat quis pariatur. In pariatur consequat aliquip adipisicing sunt. Nisi occaecat adipisicing Lorem qui. Id eiusmod adipisicing qui ut amet qui est esse pariatur magna esse cillum.</p>
+            <p>Eu cupidatat ullamco velit non consequat exercitation qui aliqua fugiat aute. Excepteur deserunt do in laboris officia velit proident ut. Consectetur aliqua et excepteur ea ullamco non eu. Nostrud ex aliqua irure tempor ullamco fugiat excepteur aliqua occaecat anim excepteur nostrud nostrud pariatur.</p>
+            <p>Anim culpa adipisicing quis tempor. Eiusmod deserunt anim sunt duis ad laboris eiusmod incididunt. Nostrud ullamco minim ad tempor minim commodo ipsum eu qui duis do.</p>
+            <p>Veniam tempor deserunt pariatur mollit excepteur aute. Ex Lorem velit quis nostrud ad nulla irure adipisicing ut ea proident occaecat ad esse. Est eu elit esse excepteur minim est qui aute est tempor ea aliqua et do. Sunt ea magna aliquip eiusmod excepteur duis veniam ullamco. Pariatur veniam nulla excepteur mollit eiusmod voluptate enim ea. Ad labore culpa ullamco enim pariatur. Est ad elit cupidatat et excepteur aliquip sit qui.</p>
+            <p>Voluptate magna eu esse in cillum laborum do elit aliqua. Aliqua ex culpa occaecat ut id ullamco minim fugiat qui nulla. Irure labore eu veniam anim eiusmod veniam laborum dolor velit nostrud ullamco ea labore. Cupidatat commodo deserunt veniam eu consectetur deserunt eiusmod labore excepteur. Lorem proident sunt do deserunt aliquip. Adipisicing nulla eu exercitation commodo ad id quis officia Lorem voluptate laboris sunt est aute.</p>
+            </section>
+            <section id="exhibits">
+            <h1>Exposiciones</h1>
+            <p><span className="year">2018</span> - Ullamco culpa quis labore est.</p>
+            <p><span className="year">2017</span> - Cillum sit ut pariatur aute magna excepteur labore nostrud elit.</p>
+            <p><span className="year">2015</span> - Consectetur deserunt aliqua ipsum esse sunt culpa eu ullamco.</p>
+            <p><span className="year">2010</span> - Tempor mollit ipsum Lorem ipsum incididunt ea occaecat est.</p>
+            </section>
+        </section>
+    );
+};
+
+export default Biography;
