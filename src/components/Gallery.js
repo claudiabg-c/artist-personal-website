5188eb958728a6604d1db52523fcc8cfa0bcaeb8
Claudia Berrocal <claudia.berrocalgomez@gmail.com>
2023-10-22 13:38:05 +0200

    Add gallery component.

---
+++ src/components/Gallery.js
@@ -0,0 +1,37 @@
+function Gallery() {
+    return (
+        <section className="gallery">
+            <h1>Galería</h1>
+            <section class="oil">
+                <h2>Óleos</h2>
+                <div>
+                    <img/>
+                    <p></p>
+                </div>
+            </section>
+            <section className="acrylic">
+                <h2>Acrílicos</h2>
+                <div>
+                    <img/>
+                    <p></p>
+                </div>
+            </section>
+            <section className="engravings">
+                <h2>Grabados</h2>
+                <div>
+                    <img/>
+                    <p></p>
+                </div>
+            </section>
+            <section className="other-jobs">
+                <h2>Otros trabajos</h2>
+                <div>
+                    <img/>
+                    <p></p>
+                </div>
+            </section>
+        </section>
+    );
+};
+
+export default Gallery;
