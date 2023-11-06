0a5299e8f1c4dbbe6aed7782e5c52e38d3d0159e
Claudia Berrocal <claudia.berrocalgomez@gmail.com>
2023-11-06 19:05:40 +0100

    Add biography and exhibits literals

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -2,19 +2,48 @@ function Biography() {
     return (
         <section className="biography-and-exibits">
             <section id="biography">
-            <h1>Biografía</h1>
-            <p>Culpa minim aliqua mollit sit aute incididunt in deserunt elit consequat quis pariatur. In pariatur consequat aliquip adipisicing sunt. Nisi occaecat adipisicing Lorem qui. Id eiusmod adipisicing qui ut amet qui est esse pariatur magna esse cillum.</p>
-            <p>Eu cupidatat ullamco velit non consequat exercitation qui aliqua fugiat aute. Excepteur deserunt do in laboris officia velit proident ut. Consectetur aliqua et excepteur ea ullamco non eu. Nostrud ex aliqua irure tempor ullamco fugiat excepteur aliqua occaecat anim excepteur nostrud nostrud pariatur.</p>
-            <p>Anim culpa adipisicing quis tempor. Eiusmod deserunt anim sunt duis ad laboris eiusmod incididunt. Nostrud ullamco minim ad tempor minim commodo ipsum eu qui duis do.</p>
-            <p>Veniam tempor deserunt pariatur mollit excepteur aute. Ex Lorem velit quis nostrud ad nulla irure adipisicing ut ea proident occaecat ad esse. Est eu elit esse excepteur minim est qui aute est tempor ea aliqua et do. Sunt ea magna aliquip eiusmod excepteur duis veniam ullamco. Pariatur veniam nulla excepteur mollit eiusmod voluptate enim ea. Ad labore culpa ullamco enim pariatur. Est ad elit cupidatat et excepteur aliquip sit qui.</p>
-            <p>Voluptate magna eu esse in cillum laborum do elit aliqua. Aliqua ex culpa occaecat ut id ullamco minim fugiat qui nulla. Irure labore eu veniam anim eiusmod veniam laborum dolor velit nostrud ullamco ea labore. Cupidatat commodo deserunt veniam eu consectetur deserunt eiusmod labore excepteur. Lorem proident sunt do deserunt aliquip. Adipisicing nulla eu exercitation commodo ad id quis officia Lorem voluptate laboris sunt est aute.</p>
+                <h1>Biografía</h1>
+                <p>Licenciada en Bellas Artes, graduada con distinción en las secciones de Pintura en 1983 y de Grabado en 1997, Gloria es un ejemplo de dedicación y pasión por el mundo del arte. Sus estudios superiores tuvieron lugar en la prestigiosa Facultad de Bellas Artes de San Fernando, perteneciente a la Universidad Complutense de Madrid, donde floreció como artista y educadora.</p>
+                <p>Complementando su formación, obtuvo el Curso de Aptitud Pedagógica (CAP) en la Facultad de Ciencias de la Educación, preparándose para inspirar y guiar a las generaciones venideras. Su compromiso con la enseñanza se cristalizó al ganar un concurso-oposición para el cuerpo de profesores de enseñanzas secundarias, lo que le permitió impartir materias como Dibujo artístico y científico, Educación Visual, Grabado, Imagen y Expresión, y Comunicación Audiovisual. Desde 1984 hasta el presente, ha compartido su conocimiento y pasión con sus alumnos, dejando una huella imborrable en el mundo educativo.</p>
+                <p>Además de su valiosa labor docente, también persiguió su crecimiento académico a través de cursos de doctorado en 1984-85. Durante este tiempo, se embarcó en una emocionante investigación que culminó en su tesis doctoral titulada "Análisis de la forma y el color en la pintura de Ángel Lizcano", contribuyendo así al enriquecimiento del entendimiento artístico.</p>
+                <p>Su vida y carrera son testimonios vivos de la pasión y el compromiso que ha puesto en el mundo del arte y la educación, dejando una marca perdurable en ambos campos.</p>
             </section>
             <section id="exhibits">
-            <h1>Exposiciones</h1>
-            <p><span className="year">2018</span> - Ullamco culpa quis labore est.</p>
-            <p><span className="year">2017</span> - Cillum sit ut pariatur aute magna excepteur labore nostrud elit.</p>
-            <p><span className="year">2015</span> - Consectetur deserunt aliqua ipsum esse sunt culpa eu ullamco.</p>
-            <p><span className="year">2010</span> - Tempor mollit ipsum Lorem ipsum incididunt ea occaecat est.</p>
+                <h1>Exposiciones y premios</h1>
+                <p><span className="year">1973</span> - Subasta-exposición en la Galería de Arte Rottemburg de Madrid.</p>
+                <p><span className="year">1976</span> - Exposición colectiva en la Casa Municipal de Talavera de la Reina.</p>
+                <p><span className="year">1977</span> - Exposición de mini-cuadros en la Galería Círculo II de Madrid.</p>
+                <p><span className="year">1978</span> - Exposición colectiva en la Galería de Arte Durero de Madrid.</p>
+                <p><span className="year">1986</span> - Exposición individual en la Galería de Arte Jandro en P. de Mallorca.</p>
+                <p><span className="year">1987</span> - Exposición colectiva en el Museo F.Juan Cobo de Alcazar de S Juan.</p>
+                <p><span className="year">1987</span> - Concurso-exposición de pintura de Ministerio de Educación y Ciencia.</p>
+                <p><span className="year">1987</span> - Exposición colectiva en la Galería de Arte Millares de Madrid.</p>
+                <p><span className="year">1988</span> - Exposición individual en la Galería de Arte Millares de Madrid.</p>
+                <p><span className="year">1989</span> - Exposición individual en la Galería de Arte Millares de Madrid.</p>
+                <p><span className="year">1989</span> - Exposición colectiva en la Galería de Arte Tioda de Gijón.</p>
+                <p><span className="year">1990</span> - 2º Premio de pintura de Aldea del Rey de Ciudad Real.</p>
+                <p><span className="year">1990</span> - 2º Premio a la investigación Giner de los Rios. Madrid.</p>
+                <p><span className="year">1990</span> - Exposición individual en la Galería de Arte Hierro y Azul de Oviedo.</p>
+                <p><span className="year">1990</span> - Exposición colectiva en la Galería de Arte Tioda de Gijón.</p>
+                <p><span className="year">1992</span> - Exposición individual en el Museo Municipal de Alcázar de S. Juan.</p>
+                <p><span className="year">1995</span> - Premio al Cartel del día de la Mujer. Alcázar de S. Juan.</p>
+                <p><span className="year">1993</span> - Exposición colectiva en la Casa de la Cultura de Alcázar de S. Juan.</p>
+                <p><span className="year">1995</span> - 1er Premio. Cartel Fiestas de Moros y Cristianos. Alcázar de S. Juan.</p>
+                <p><span className="year">1997</span> - Exposición colectiva de grabados en la Facultad de BB.AA. Madrid.</p>
+                <p><span className="year">1998</span> - Exposición colectiva de grabados en la Facultad de BB.AA. Madrid.</p>
+                <p><span className="year">2000</span> - Cartel anunciador de las Fiestas Patronales de Alcázar de S. Juan.</p>
+                <p><span className="year">2002</span> - Exposición de grabados en la Sede social de Caja Mar. Málaga.</p>
+                <p><span className="year">2008</span> - Exposición de grabados en Museo Municipal de Alcázar de S. Juan.</p>
+                <p><span className="year">2008</span> - Exposición de pintura y grabados en el Museo de Arte Moderno de Hellín.</p>
+                <p><span className="year">2008</span> - 4º Concurso – Exposición de pintura. Muface. Madrid.</p>
+                <p><span className="year">2009</span> - 1er Concurso – Exposición de grabados. MUFAR. Muface. Madrid.</p>
+                <p><span className="year">2009</span> - Exposición de obra gráfica en la XVII edición de Estampa. Ifema. Madrid.</p>
+                <p><span className="year">2010</span> - Exposición de pintura y grabados en FAIN. P de Congresos y Exposiciones.</p>
+                <p><span className="year">2010</span> - Exposición de pintura y grabados en la galería EspacioRonda de Madrid.</p>
+                <p><span className="year">2011</span> - Exposición en la Feria internacional del grabado. ESTAMPA. Matadero. Madrid.</p>
+                <p><span className="year">2013</span> - Exposición en la primera Feria alicantina de Libros de Artista. FALA. Alicante.</p>
+                <p><span className="year">2014</span> - Exposición colectiva en Eka & Moor Gallery en Madrid.</p>
+
             </section>
         </section>
     );
